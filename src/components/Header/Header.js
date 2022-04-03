18fbf2e7036f1ec098d962ffac9a7f093737c5cf
shariarhossain23e <shariarhossain23@gamil.comm>
2022-04-04 01:27:28 +0600

    hambarger menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,16 @@
-import React from "react";
+import { MenuIcon, XIcon } from '@heroicons/react/solid';
+import React, { useState } from "react";
+import { } from 'react-icons/fa';
 import CustomLink from "../Customlink/CustomLink";
-
 const Header = () => {
+  const [open,setOpen] = useState(false)
   return (
     <div className="">
-       <CustomLink className="text-lg font-semibold " to="/">Home</CustomLink>
+      <div onClick={()=> setOpen(!open)} className='h-6 w-6 md:hidden'>{open ? <XIcon></XIcon> : <MenuIcon></MenuIcon>}</div>
+      <div className={`md:flex justify-center  absolute w-full duration-500 ease-in-out md:static ${open ?"top-6" :"top-[-200px]"}`}>
+      <CustomLink className="text-lg font-semibold md:mx-4 " to="/">Home</CustomLink>
        <CustomLink className='text-lg font-semibold' to='review'> Reviews</CustomLink>
+      </div>
     </div>
   );
 };

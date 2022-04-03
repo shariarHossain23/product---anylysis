ae26bd56651563e86ed261669ada8bf07c83f306
shariarhossain23e <shariarhossain23@gamil.comm>
2022-04-03 21:37:42 +0600

    setup

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import ReactDOMClient from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
+ const conatainer = document.getElementById('root');
+ const root = ReactDOMClient.createRoot(conatainer)
+root.render(
+  <BrowserRouter>
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function

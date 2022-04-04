500170f1c02fba168c7153faeeb2bac48dce2358
shariarhossain23e <shariarhossain23@gamil.comm>
2022-04-04 13:02:04 +0600

    responsive

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
 
 const About = () => {
-    return (
-        <div>
-            <h1>about section</h1>
-        </div>
-    );
+  return (
+    <div className="mx-auto w-3/4 mt-6">
+      <h1 className="text-center mb-10 text-4xl">About us</h1>
+      <p className="text-xl">
+        The About Us page of your website is an essential source of information
+        for all who want to know more about your business. About Us pages are
+        where you showcase your history, what is unique about your work, your
+        company’s values, and who you serve. The design, written content, and
+        visual or video elements together tell an important story about who you
+        are and why you do it. How can you make the most of this integral part
+        of your marketing strategy? In this article, you’ll learn what makes an
+        exceptional About Us page. You’ll find 25 examples of the best ones out
+        there, too, to inspire your own About Us page design and content.
+      </p>
+    </div>
+  );
 };
 
 export default About;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <div className="shadow p-6">
       <div onClick={()=> setOpen(!open)} className='h-6 w-6 md:hidden'>{open ? <XIcon></XIcon> : <MenuIcon></MenuIcon>}</div>
-      <div className={`md:flex justify-center absolute left-auto w-full duration-500 ease-in-out md:static ${open ?"top-12" :"top-[-220px]"}`}>
+      <div className={`md:flex justify-center absolute  w-3/4 mx-auto duration-500 ease-in-out md:static ${open ?"top-16 bg-indigo-500 text-white text-center p-4" :"top-[-220px]"} `}>
       <CustomLink className="text-lg font-semibold md:mx-4 " to="/">Home</CustomLink>
        <CustomLink className='text-lg font-semibold md:mx-4' to='review'> Reviews</CustomLink>
        <CustomLink className='text-lg font-semibold md:mx-4' to='/dashboard'> Dashboard</CustomLink>
